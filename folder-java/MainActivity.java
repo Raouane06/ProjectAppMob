2c0330233b8dfe9e67df494df748fdfa9d79e877
Meriem <152006164+merry122@users.noreply.github.com>
2025-03-09 22:42:04 +0100

    Update MainActivity.java

--- folder-java/MainActivity.java
+++ folder-java/MainActivity.java
@@ -1,22 +1,24 @@
-package com.example.lab1;
+package com.example.merry22;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewModules;
     private ModuleAdapter moduleAdapter;
     private List<Module> modules;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         recyclerViewModules = findViewById(R.id.recyclerViewModules);
         recyclerViewModules.setLayoutManager(new LinearLayoutManager(this));
 
@@ -25,5 +27,26 @@ public class MainActivity extends AppCompatActivity
 
         moduleAdapter = new ModuleAdapter(modules);
         recyclerViewModules.setAdapter(moduleAdapter);
+
+        Button btnCalculate = findViewById(R.id.btnCalculate);
+        btnCalculate.setOnClickListener(view -> {
+            double sumWeightedAverages = 0;
+            int sumCoefficients = 0;
+
+            for (Module module : modules) {
+                double moduleAverage = ((module.getTd() + module.getTp()) / 2 + module.getExam()) / 2;
+                sumWeightedAverages += moduleAverage * module.getCoefficient();
+                sumCoefficients += module.getCoefficient();
+            }
+
+            double weightedAverage = sumWeightedAverages / sumCoefficients;
+            TextView tvResult = findViewById(R.id.tvResult);
+            if (weightedAverage >= 10) {
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.green));
+            } else {
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.red));
+            }
+            tvResult.setText("Result: " + weightedAverage + (weightedAverage >= 10 ? " (Pass)" : " (Fail)"));
+        });
     }
 }
