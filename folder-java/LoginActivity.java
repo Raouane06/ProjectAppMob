a7a1b0556787ff5b1d22d54a3dc01da402f63b09
Raouane <162448387+Raouane06@users.noreply.github.com>
2025-03-10 10:07:26 +0100

    Update LoginActivity.java

--- folder-java/LoginActivity.java
+++ folder-java/LoginActivity.java
@@ -1,54 +1,55 @@
-package com.example.merry22;
+package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
-import com.example.logsignsqlpractice.databinding.ActivityLoginBinding;
+import com.example.lab1.databinding.LoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ActivityLoginBinding binding;
-    DatabaseHelper databaseHelper;
+    LoginBinding binding;
+    database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        
+        binding = LoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        databaseHelper = new DatabaseHelper(this);
-
-        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+        db = new database(this); 
+        
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = binding.loginEmail.getText().toString();
-                String password = binding.loginPassword.getText().toString();
+                String username = binding.usernameInput.getText().toString(); 
+                String password = binding.passwordInput.getText().toString(); 
 
-                if(email.equals("")||password.equals(""))
+               
+                if (username.isEmpty() || password.isEmpty()) {
                     Toast.makeText(LoginActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+                } else {
+                    
+                    Boolean checkCredentials = db.checkUserPass(username, password);
 
-                    if(checkCredentials == true){
-                        Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
-                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
+                    if (checkCredentials) {
+                        Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
-                    }else{
+                        finish(); 
+                    } else {
                         Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
-
         binding.signupRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
+                finish(); 
             }
         });
     }
