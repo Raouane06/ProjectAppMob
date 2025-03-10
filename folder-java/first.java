57f9a92346d81473427d92db85c1df92e5c2bf92
Raouane <162448387+Raouane06@users.noreply.github.com>
2025-03-10 10:09:08 +0100

    Update first.java

--- folder-java/first.java
+++ folder-java/first.java
@@ -5,18 +5,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class first extends AppCompatActivity {
-    private static final int SPLASH_TIME_OUT = 2000; // 2 seconds
+public class first extends AppCompatActivity
+{
+    private static final int SPLASH_TIME_OUT = 2000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first);
 
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(first.this, MainActivity.class);
+            Intent intent = new Intent(first.this, SignupActivity.class);
             startActivity(intent);
-            finish(); // Close SplashActivity
+            finish();
         }, SPLASH_TIME_OUT);
     }
 }
