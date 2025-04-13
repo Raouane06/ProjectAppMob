002ed8ba42330045307cc63fa580433519122573
Raouane <162448387+Raouane06@users.noreply.github.com>
2025-04-13 23:16:22 +0100

    Update LoginActivity.java

--- folder-java/LoginActivity.java
+++ folder-java/LoginActivity.java
@@ -1,55 +1,62 @@
-package com.example.lab1;
+package com.example.labb1;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-import com.example.lab1.databinding.LoginBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.labb1.databinding.LoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     LoginBinding binding;
     database db;
+    private static final String USER_PREFS = "user_prefs";
+    private static final String USERNAME_KEY = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         binding = LoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        db = new database(this); 
-        
+        db = new database(this);
+
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = binding.usernameInput.getText().toString(); 
-                String password = binding.passwordInput.getText().toString(); 
+                String username = binding.usernameInput.getText().toString();
+                String password = binding.passwordInput.getText().toString();
 
-               
                 if (username.isEmpty() || password.isEmpty()) {
                     Toast.makeText(LoginActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
                 } else {
-                    
-                    Boolean checkCredentials = db.checkUserPass(username, password);
+                    Boolean checkCredentials = db.checkUserCredentials(username, password);
 
                     if (checkCredentials) {
+                        SharedPreferences.Editor editor = getSharedPreferences(USER_PREFS, MODE_PRIVATE).edit();
+                        editor.putString(USERNAME_KEY, username);
+                        editor.apply(); 
+
                         Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
-                        finish(); 
+                        finish();
                     } else {
                         Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
+
         binding.signupRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
-                finish(); 
+                finish();
             }
         });
     }
