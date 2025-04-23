caae95deb8a43f8f0ce1986539d933406af2d403
Raouane Krama <162448387+Raouane06@users.noreply.github.com>
2025-04-23 21:57:09 +0100

    Update AnnouncementDBHelper.java

--- folder-java/AnnouncementDBHelper.java
+++ folder-java/AnnouncementDBHelper.java
@@ -1,20 +1,23 @@
-package com.example.myapplication;
+package com.example.labb1;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class AnnouncementDBHelper extends SQLiteOpenHelper {
-
     private static final String DB_NAME = "User.db";
     private static final int DB_VERSION = 1;
     private static final String TABLE_NAME = "announcements";
     private static final String COLUMN_ID = "id";
-    private static final String COLUMN_TEXT = "text";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_CONTENT = "content";
+    private static final String COLUMN_DATE = "date";
+    private static final String COLUMN_AUTHOR = "author";
 
     public AnnouncementDBHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -24,30 +27,41 @@ public class AnnouncementDBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + COLUMN_TEXT + " TEXT NOT NULL)");
+                + COLUMN_TITLE + " TEXT NOT NULL, "
+                + COLUMN_CONTENT + " TEXT NOT NULL, "
+                + COLUMN_DATE + " TEXT NOT NULL, "
+                + COLUMN_AUTHOR + " TEXT NOT NULL)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Not needed now
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 
-    public void addAnnouncement(String text) {
+    public void addAnnouncement(Announcement announcement) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_TEXT, text);
+        values.put(COLUMN_TITLE, announcement.getTitle());
+        values.put(COLUMN_CONTENT, announcement.getContent());
+        values.put(COLUMN_DATE, announcement.getDate());
+        values.put(COLUMN_AUTHOR, announcement.getAuthor());
         db.insert(TABLE_NAME, null, values);
         db.close();
     }
 
-    public ArrayList<String> getAllAnnouncements() {
-        ArrayList<String> announcements = new ArrayList<>();
+    public ArrayList<Announcement> getAllAnnouncements() {
+        ArrayList<Announcement> announcements = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " ORDER BY id DESC", null);
 
         if (cursor.moveToFirst()) {
             do {
-                announcements.add(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TEXT)));
+                announcements.add(new Announcement(
+                        cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_DATE)),
+                        cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_AUTHOR))));
             } while (cursor.moveToNext());
         }
         cursor.close();
